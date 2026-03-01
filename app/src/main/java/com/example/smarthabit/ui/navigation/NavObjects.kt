7f124a1f6c45416d653848ea2edac2c5c625c3af
norbert-kr <norbert.kr001@gmail.com>
2026-03-01 19:38:13 +0000

    MAD-Kotlin_Android/Screens-and-navigation - set up initial navigation

---
+++ app/src/main/java/com/example/smarthabit/ui/navigation/NavObjects.kt
@@ -0,0 +1,20 @@
+package com.example.smarthabit.ui.navigation
+
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.serialization.NavKeySerializer
+import kotlinx.serialization.Serializable
+
+
+
+sealed interface NavObjects : NavKey {
+
+    @Serializable
+    data object LoginScreen : NavObjects, NavKey
+
+    @Serializable
+    data object ListHabitScreen : NavObjects, NavKey
+
+    @Serializable
+    data object AddHabitScreen : NavObjects, NavKey
+
+}
